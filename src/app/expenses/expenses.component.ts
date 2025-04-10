8f9d4fd631f7cc8f60a6a428409059717306a2db
ShockRate <blackhound05@yahoo.gr>
2025-04-10 23:06:48 +0300

    Added Boostrap to Expenses

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @Component({
   selector: 'app-expenses',
   standalone:true,
-  imports: [],
+  imports: [PaginationComponent],
   templateUrl: './expenses.component.html',
   styleUrl: './expenses.component.css'
 })
@@ -20,7 +21,7 @@ export class ExpensesComponent implements OnInit{
   private getAllExpenses(){
     this.http.get<any>('/api/expenses').subscribe({
       next: (data: any) => {
-        //console.log(data);
+        console.log(data);
         this.data = data.data;
       }, error: (err) => console.log(err)
     });
---
+++ src/app/expenses/pagination/pagination.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+
+totalItems = input<any>();
+
+}
